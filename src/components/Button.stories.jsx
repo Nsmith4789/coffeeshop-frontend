3e95128bd9f9bf38ebf798957ebd048aade6fde2
Nsmith4789 <156531219+Nsmith4789@users.noreply.github.com>
2025-01-15 18:52:38 -0600

    added storybook and daisyui and components

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,19 @@
+import Button from './Button';
+
+const meta = {
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    label: "Purchase Now!"
+  }
+};
+
+export const PurchaseLater = {
+  args: {
+    label: "Purchase Later!!!!"
+  }
+};
---
+++ src/components/testbutton.stories.jsx
@@ -0,0 +1,9 @@
+import Testbutton from './testbutton';
+
+const meta = {
+  component: Testbutton,
+};
+
+export default meta;
+
+export const Default = {};
